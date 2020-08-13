7a6cf72bf24609ce4e81eac629c0cf6344dad7fb
GHOST <ghostdevbusiness@gmail.com>
2020-08-13 17:25:41 +0100

    Made it so that passing no args to parse doesn't throw an error

--- src/parse.js
+++ src/parse.js
@@ -1,9 +1,8 @@
 const configManager = require('./config.js');
 const DashArgs = require('./dashargs.js');
 
-module.exports = (string, options = {}) => {
+module.exports = (string = '', options = {}) => {
 
-    if (!string) throw new Error('ERR: dashargs#parse - must provide some args to parse');
     if ((typeof string != 'string')) throw new TypeError('ERR: dashargs#parse - given args must be a string');
     if ((typeof options != 'object')) throw new TypeError('ERR: dashargs#parse - given options must be an object')
 
