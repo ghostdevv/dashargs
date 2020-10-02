8a3363f9571c05ba512d6dfd0a74224d5a7e3256
GHOST <ghostdevbusiness@gmail.com>
2020-10-02 15:29:23 +0100

    Moved index.js into src

--- index.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    parse: require('./src/parse.js'),
-    config: require('./src/config.js').set,
-    DashArgs: require('./src/dashargs.js')
-};
---
+++ src/index.js
@@ -0,0 +1,5 @@
+module.exports = {
+    parse: require('./parse.js'),
+    config: require('./config.js').set,
+    DashArgs: require('./dashargs.js')
+};
