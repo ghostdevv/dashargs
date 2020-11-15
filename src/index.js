96eae84e501a5e4f6e9bfd958543faac94ad0610
GHOST <ghostdevbusiness@gmail.com>
2020-11-15 21:26:17 +0000

    Exposed strip method

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 module.exports = {
     parse: require('./methods').parse,
-    config: require('./options').Options.defaults
+    config: require('./options').Options.defaults,
+    strip: require('./methods').strip,
 };
