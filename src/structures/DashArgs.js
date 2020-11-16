f17e364eecbac746da94f99f5e75ca5220d959f7
GHOST <ghostdevbusiness@gmail.com>
2020-11-16 20:45:12 +0000

    Fix for issue #6

--- src/structures/DashArgs.js
+++ src/structures/DashArgs.js
@@ -104,7 +104,7 @@ module.exports = class DashArgs {
     static _sanitize(item, type) {
         switch(type) {
             case 'key':
-                return (typeof item == 'string') ? item.replace(/-/g, '') : item;
+                return (typeof item == 'string') ? item.replace(/^-+/g, '') : item;
                 break;
             case 'value':
                 return ((typeof item == 'string') && item.match(/(?:^"[^]+"$)|(?:^'[^]+'$)/gim)) ? item.slice(1, -1) : item;
