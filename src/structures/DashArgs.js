c7c25a06fdab68f65558a35dbae025ea1928cab4
GHOST <ghostdevbusiness@gmail.com>
2020-11-17 21:21:04 +0000

    Has now uses get

--- src/structures/DashArgs.js
+++ src/structures/DashArgs.js
@@ -17,7 +17,7 @@ module.exports = class DashArgs {
     has(key) {
         if (!key) throw new SyntaxError('dashargs#parse(has) - must provide a key: <parsed-args>.has(\'key\')');
         if ((typeof key != 'string')) throw new TypeError('dashargs#parse(has) - given key must be a string');
-
+        
         return !!this[key];
     };
 
