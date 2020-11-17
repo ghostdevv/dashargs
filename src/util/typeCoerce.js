d4664d7d2acdef0ad651c0323d51842226269014
GHOST <ghostdevbusiness@gmail.com>
2020-11-17 20:38:30 +0000

    Ability to parse JSON

--- src/util/typeCoerce.js
+++ src/util/typeCoerce.js
@@ -6,6 +6,19 @@ module.exports = (item = '') => {
     if (!isNaN(Number(item))) return Number(item);
     if (item.toLowerCase() == 'true') return true;
     if (item.toLowerCase() == 'false') return false;
+
+    const object = tryJSON(item);
+    if (object) return object;
     
     return item;
+};
+
+function tryJSON(string) {
+    try {
+        const object = JSON.parse(string);
+        return object;  
+    }
+    catch {
+        return undefined;
+    };
 };
