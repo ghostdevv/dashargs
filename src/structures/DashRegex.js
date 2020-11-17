50b1300b5e7530730f483f5e1c6ce07b868f22e0
GHOST <ghostdevbusiness@gmail.com>
2020-11-17 20:29:33 +0000

    New regex patterns

--- src/structures/DashRegex.js
+++ src/structures/DashRegex.js
@@ -7,8 +7,16 @@ module.exports = class DashRegex {
         return new RegExp(DashRegex.patternString.replace(new RegExp(DashRegex.standardPrefix, 'gi'), this.prefix), DashRegex.flags);
     };
 
+    getKeyIdentifier() {
+        return new RegExp(DashRegex.keyIdentifierPatternString.replace(new RegExp(DashRegex.standardPrefix, 'gi'), this.prefix), DashRegex.flags);
+    };
+
     static get patternString() {
         return `(--[^ \\n]+)|(-(?:([^-\\s])+)( )?(?:('(?:\\.|[^'])*'|"(?:\\.|[^"])*")|((?:\\.|[^- \\n])*)?))`;
+    };
+
+    static get keyIdentifierPatternString() {
+        return `^(?:(--[^ \n]+)|(-(?:([^-\\s])+)))`;
     };
 
     static get flags() {
