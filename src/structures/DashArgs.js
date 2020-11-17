c9856048dcbb16675cbe26a6a210730e1ab304e7
GHOST <ghostdevbusiness@gmail.com>
2020-11-17 19:19:17 +0000

    Implimented new Parser

--- src/structures/DashArgs.js
+++ src/structures/DashArgs.js
@@ -1,10 +1,10 @@
-const { identify, sanitize, getKeyVal, typeCoerce } = require('../util');
+const Parser = require('./Parser.js');
 
 module.exports = class DashArgs {
     #parsed;
 
     constructor(string, config = {}) {
-        this.#parsed = DashArgs._parse(string, config);
+        this.#parsed = new Parser(string, config).parse();
 
         this.#parsed.forEach(({ key, value }) => {
             if (config.unique) {
@@ -28,39 +28,5 @@ module.exports = class DashArgs {
 
     array() {
         return this.#parsed.map(({ key, value, raw }) => ({ key, value, raw }));
-    };
-
-    static _parse(string, config) {
-        const pattern = /(--[^ \n]+)|(-(?:([^-\s])+)( )?(?:('(?:\.|[^'])*'|"(?:\.|[^"])*")|((?:\.|[^- \n])*)?))/gim;
-        let matches = (string.match(pattern) || []).map(m => DashArgs._parseSingle(m, config)).flat();
-        if (!config.parseFlags) matches = matches.filter(m => !m.type.match(/flag/gim));
-        if (!config.parseArgs) matches = matches.filter(m => !m.type.match(/arg/gim));
-        if (config.typeCoerce) matches = matches.map(m => ({ ...m, value: typeCoerce(m.value) }));
-        return matches;
-    };
-    
-    static _parseSingle(string) {
-        const { key, value } = getKeyVal(string);
-        const { type } = identify(string);
-        const raw = string;
-
-        switch(type) {
-            case 'arg':
-                return({ key: sanitize(key, 'key'), value, type, raw });
-                break;
-            case 'compound-arg':
-                return({ key: sanitize(key, 'key'), value: sanitize(value, 'value'), type, raw });
-                break;
-            case 'flag':
-                if (sanitize(key, 'key').length == 1) return ({ key: sanitize(key, 'key'), value: true, type, raw });
-                return sanitize(key, 'key').split('').map(k => `-${k}`).map(k => DashArgs._parseSingle(k))
-                break;
-            case 'compound-flag':
-                return({ key: sanitize(key, 'key'), value: true, type, raw })
-                break;
-            default:
-                return undefined;
-                break;
-        };
     };
 };
