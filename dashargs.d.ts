e1c43c673b007b25e42746603064c8de472a26a4
GHOST <ghostdevbusiness@gmail.com>
2020-12-04 21:42:07 +0000

    Added parse function and DashArgs class declaration

--- dashargs.d.ts
+++ dashargs.d.ts
@@ -12,4 +12,27 @@ declare module 'dashargs' {
         typeCoerce: boolean,
         prefix: string,
     }): void;
+
+    export class DashArgs {
+        constructor(string: string, config?: {
+            unique: boolean,
+            parseFlags: boolean,
+            parseArgs: boolean,
+            typeCoerce: boolean,
+            prefix: string,
+        });
+        get string(): string;
+        get config(): object;
+        has(key: string): boolean;
+        get(key: string): any;
+        array(): Array<object>;
+    }
+
+    export function parse(string: string, options?: {
+        unique: boolean,
+        parseFlags: boolean,
+        parseArgs: boolean,
+        typeCoerce: boolean,
+        prefix: string,
+    }): DashArgs
 }
