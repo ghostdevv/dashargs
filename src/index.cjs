1531d0e1da1bcfbb7fbd31d6dacb1de73f55e841
GHOST <ghostdevbusiness@gmail.com>
2020-12-04 15:38:28 +0000

    Reenabled methods

--- src/index.cjs
+++ src/index.cjs
@@ -1,9 +1,5 @@
-// module.exports = {
-//     parse: require('./methods').parse,
-//     config: require('./options').Options.defaults,
-//     strip: require('./methods').strip,
-// };
-
 module.exports = {
-    hello: true
-}
+    parse: require('./methods').parse,
+    config: require('./options').Options.defaults,
+    strip: require('./methods').strip,
+};
