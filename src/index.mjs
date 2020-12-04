d54c666a8ee5caf6573e760ad680be46258d5115
GHOST <ghostdevbusiness@gmail.com>
2020-12-04 15:46:51 +0000

    Module support index file

---
+++ src/index.mjs
@@ -0,0 +1,9 @@
+import config from './options/Options.js';
+import parse from './methods/parse.js';
+import strip from './methods/strip.js';
+
+export default {
+    parse,
+    config: config.defaults,
+    strip
+};
