32fb20a9cf75fff1245836cb03988abb3b08868d
GHOST <ghostdevbusiness@gmail.com>
2020-12-21 14:09:53 +0000

    fix: updated strip method
    Strip method now uses new options sytem fixing issue #17

--- src/methods/strip.js
+++ src/methods/strip.js
@@ -1,21 +1,25 @@
+const { Options } = require('../options');
 const parse = require('./parse.js');
 
-module.exports = (string = '', { removeWhitespace, removeFlags, removeArgs, prefix } = { removeWhitespace: true, removeFlags: true, removeArgs: true }) => {
-    if ((typeof string != 'string')) throw new TypeError('Expected type string dashargs#strip');
+module.exports = (string = '', options = {}) => {
+    if (typeof string != 'string')
+        throw new TypeError('Expected type string dashargs#strip');
+
+    options = Object.assign(new Options('strip'), options);
 
     const args = parse(string, {
         typeCoerce: false,
         unique: false,
-        parseArgs: removeArgs,
-        parseFlags: removeFlags,
-        prefix: !prefix ? '-' : prefix
+        parseArgs: options.removeArgs,
+        parseFlags: options.removeFlags,
+        prefix: options.prefix,
     })
-    .array()
-    .map(x => (removeWhitespace ? ' ' : '') + x.raw.trim());
+        .array()
+        .map((x) => (options.removeWhitespace ? ' ' : '') + x.raw.trim());
 
     for (const raw of args) {
         string = string.replace(raw, '').replace(raw.trim(), '');
-    };
+    }
 
     return string;
 };
