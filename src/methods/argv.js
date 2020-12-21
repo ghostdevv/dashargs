0a84ae8e862efb027aae49730881c569e2ee9614
GHOST <ghostdevbusiness@gmail.com>
2020-12-21 19:16:43 +0000

    fix: argv function didn't preserve quotes like expected

--- src/methods/argv.js
+++ src/methods/argv.js
@@ -1,5 +1,11 @@
 const parse = require('./parse.js');
 
 module.exports = (opt = {}) => {
-    return parse(process.argv.slice(2).join(' '), opt);
+    return parse(
+        process.argv
+            .slice(2)
+            .map((x) => (x.match(/ /gm) ? `"${x}"` : x))
+            .join(' '),
+        opt,
+    );
 };
