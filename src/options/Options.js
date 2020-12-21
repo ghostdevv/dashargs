82684eb7630408a763df7aecb2a810900b40979c
GHOST <ghostdevbusiness@gmail.com>
2020-12-21 13:59:38 +0000

    refactor: options handler rewritten

--- src/options/Options.js
+++ src/options/Options.js
@@ -1,21 +1,65 @@
-const defaultOptions = {
-    unique: true,
-    parseFlags: true,
-    parseArgs: true,
-    typeCoerce: false,
-    prefix: '-',
+'use strict';
+const options = {
+    parse: {
+        unique: true,
+        parseFlags: true,
+        parseArgs: true,
+        typeCoerce: false,
+        prefix: '-',
+    },
+    strip: {
+        removeWhitespace: true,
+        removeFlags: true,
+        removeArgs: true,
+        prefix: '-',
+    },
+};
+
+const typeMap = {
+    parse: {
+        unique: 'boolean',
+        parseFlags: 'boolean',
+        parseArgs: 'boolean',
+        typeCoerce: 'boolean',
+        prefix: 'string',
+    },
+    strip: {
+        removeWhitespace: 'boolean',
+        removeFlags: 'boolean',
+        removeArgs: 'boolean',
+        prefix: 'string',
+    },
 };
 
 module.exports = class Options {
-    constructor() {
-        this.unique = defaultOptions.unique;
-        this.parseFlags = defaultOptions.parseFlags;
-        this.parseArgs = defaultOptions.parseArgs,
-        this.typeCoerce = defaultOptions.typeCoerce;
-        this.prefix = defaultOptions.prefix;
-    };
+    constructor(type) {
+        const entries = Object.entries(type ? options[type] : options);
+        for (const [key, value] of entries) {
+            this[key] = value;
+        }
+    }
 
     static defaults(opt = {}) {
-        Object.assign(defaultOptions, opt);
-    };
+        for (const [key] of Object.entries(opt)) {
+            const item = typeMap[key],
+                optionsRef = options[key];
+
+            if (item instanceof Object) {
+                const entries = Object.entries(opt[key]);
+                for (const [key, value] of entries) {
+                    if (item[key]) {
+                        const expectedType = item[key],
+                            recievedType = typeof value;
+
+                        if (expectedType !== recievedType)
+                            throw new TypeError(
+                                `Expected type ${expectedType} for ${key}, recieved ${recievedType}`,
+                            );
+
+                        optionsRef[key] = value;
+                    }
+                }
+            }
+        }
+    }
 };
